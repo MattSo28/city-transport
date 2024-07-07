87adcd92017146a2d29c4a5c6334b4999253bb77
romahsiu <arj.matt@gmail.com>
2024-07-07 23:38:07 +0800

    Added views for core and maps

--- citytransport/core/views.py
+++ citytransport/core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from maps.views import get_geocode_city_name, generate_map
+from .forms import CityForm
 
 # Create your views here.
+
+def home(request):
+    """
+    This serves as the default home page of the web application
+    """
+    form = CityForm()
+    geocoded_city_name = None
+    
+    if request == 'POST':
+        if form.is_valid():
+            form = CityForm(request.POST)
+            input_city_text = form.cleaned_data['city_name']
+            try:
+                geocoded_city_name = get_geocode_city_name(input_city_text)
+                if not geocoded_city_name.empty:
+                    return redirect()
+
+            except Exception as e:
+                print(f"Geocoding error: {e}")
+
+    return render(request, 'home.html', {'form': form, 'city_data': geocoded_city_name})
--- citytransport/maps/views.py
+++ citytransport/maps/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
 import osmnx as ox
 import os
+import re
 
 # Create your views here.
+
+#rename filename
+def generate_filename(geocode_result):
+    # Sanitize the geocode result to create a valid filename
+    sanitized_name = re.sub(r'[^a-zA-Z0-9]+', '_', geocode_result).lower()
+    return f"{sanitized_name}_street_network"
+
 def get_geocode_city_name(input_city_text):
 
     city_name = None
@@ -11,9 +19,39 @@ def get_geocode_city_name(input_city_text):
 
     if not geocoded_city_data.empty:
         city_name = geocoded_city_data.iloc[0]['display_name']
-        
         return city_name
-
     else:
         return None
     
+def generate_map(city_name):
+    """
+    Given a geocoded city name, generate a map of the roads within the city boundaries
+    """
+    graph = ox.graph_from_place(city_name, network_type='all', simplify=True)
+
+    #Plot the street network
+    fig, ax = ox.plot_graph(
+        graph, 
+        node_size=0, 
+        bgcolor='white', 
+        edge_color='black', 
+        edge_linewidth=0.1,  # Set thin line width
+        figsize=(10, 10),  # Set a larger figure size
+        show=False, 
+        close=False
+    )
+
+    # Get the absolute path to the 'poc/static/maps' directory
+    current_script_path = os.path.dirname(os.path.abspath(__file__))
+    output_folder = os.path.join(current_script_path, 'static')
+    os.makedirs(output_folder, exist_ok=True)
+
+    #Define the filepath
+    svg_file_path = os.path.join(output_folder, generate_filename(city_name) + ".svg")
+
+    # Save the plot as an SVG file
+    fig.savefig(svg_file_path, format='svg')
+
+    return render(request, 'mapview.html', {'city_name': city_name})
+
+
