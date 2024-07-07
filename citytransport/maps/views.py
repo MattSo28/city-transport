11d092fdd579745861df88069eee2751e7e6d346
romahsiu <arj.matt@gmail.com>
2024-07-07 23:09:51 +0800

    Added geocoding function

--- citytransport/maps/views.py
+++ citytransport/maps/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import osmnx as ox
+import os
 
 # Create your views here.
+def get_geocode_city_name(input_city_text):
+
+    city_name = None
+
+    geocoded_city_data = ox.geocoder.geocode_to_gdf(input_city_text)
+
+    if not geocoded_city_data.empty:
+        city_name = geocoded_city_data.iloc[0]['display_name']
+        
+        return city_name
+
+    else:
+        return None
+    
